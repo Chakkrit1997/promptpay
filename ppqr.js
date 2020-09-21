83bd85cb39b3ca0a025ad11b8ced67b0fd8649a9
Dark_soFT <guitar_dark_soft@hotmail.com>
2020-09-21 08:01:22 +0700

    on day

--- ppqr.js
+++ ppqr.js
@@ -1,14 +1,15 @@
 
-const generatePayload = require('promptpay-qr') 
+const generatePayload = require('promptpay-qr')
 //const qrcode = require('qrcode') 
 //const fs = require('fs') 
 
-const mobileNumber = '000-000-0000' 
+const mobileNumber = '095-908-7752'
 //const IDCardNumber = '0-0000-00000-00-0'
-const amount = 0
+const amount = 10
 const payload = generatePayload(mobileNumber, { amount }) //First parameter : mobileNumber || IDCardNumber
 console.log(payload)
 
+/*
 // Convert to SVG QR Code
 const options = { type: 'svg', color: { dark: '#000', light: '#fff' } }
 qrcode.toString(payload, options, (err, svg) => {
@@ -16,3 +17,40 @@ qrcode.toString(payload, options, (err, svg) => {
     fs.writeFileSync('./qr.svg', svg)
     console.log(svg)
 })
+*/
+
+var mqtt = require('mqtt');
+
+const MQTT_SERVER = "soldier.cloudmqtt.com";
+const MQTT_PORT = "11970";
+//if your server don't have username and password let blank.
+const MQTT_USER = "snackvending";
+const MQTT_PASSWORD = "1234";
+
+// Connect MQTT
+var client = mqtt.connect({
+    host: MQTT_SERVER,
+    port: MQTT_PORT,
+    username: MQTT_USER,
+    password: MQTT_PASSWORD
+});
+
+client.on('connect', function () {
+    // Subscribe any topic
+    console.log("MQTT Connect");
+    client.subscribe('/NodeMCU', function (err) {
+        if (err) {
+            console.log(err);
+        }
+    });
+});
+
+// Receive Message and print on terminal
+client.on('message', function (topic, message) {
+    // message is Buffer
+    console.log(message.toString());
+});
+
+/*setInterval(() => {
+    client.publish("test", "hello from NodeJS");
+}, 5000);*/
